abbdc8f29e5d18f1d9155e1cdd96d26485d32337
0806socrrn <socrrn@gmail.com>
2022-03-23 13:55:49 -0500

    printf

--- _printf.c
+++ _printf.c
@@ -5,7 +5,11 @@ int _printf(const char *format, ...)
 	va_list in;	
 
 	while (format[i])
-	{	
+	{
+	if(i == '%' && i == 's')
+	{
+		_printf("String[%s]\n","hola");
+	}	
 		i++;
 	}
 	write(1, format, i);
