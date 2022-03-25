7a5a9c97410a5674d184c53b1d9a634e7acb7558
Christian <ch.ov@outlook.com>
2022-03-25 20:31:48 +0000

    betty copy

--- copy.c
+++ copy.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
+/**
+ * copy - Copies String.
+ * @in: Input of string
+ * @out: 
+ */
 void copy(char *in, const char *out)
 {
-    int i;
-    for (i = 0; out[i]; i++)
-    {
-        in[i]=out[i];
-    }
-    in[i] = '\0';
+	int i;
+	for (i = 0; out[i]; i++)
+	{
+		in[i] = out[i];
+	}
+	in[i] = '\0';
 }
