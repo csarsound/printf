3036f249d4fab8d812087a6336f73ad0d3ee94e4
Christian <ch.ov@outlook.com>
2022-03-25 20:22:19 +0000

    betty structure.c

--- structure.c
+++ structure.c
@@ -1,22 +1,28 @@
 #include "main.h"
-
+/**
+ * op_struct - Structure of functions.
+ * @st: Pointer to function.
+ * @t: Compare number of characters.
+ * Return: int(*)(va_list) 
+ */
 int (*op_struct(const char *st, int t))(va_list)
 {
-    option letters[] = {
-        {"s", conv_s},
-        {"c", conv_c},
-        {NULL, NULL}
-    };
-    
-    int i;
-    
-    for (i = 0; letters[i].specifier != NULL; i++)
-    {
-        if (letters[i].specifier[0] == st[t])
-        {
-            return (letters[i].f);
-        }
-    }
-    
-    return (0);
+	option letters[] = 
+	{
+		{"s", conv_s},
+		{"c", conv_c},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	for (i = 0; letters[i].specifier != NULL; i++)
+	{
+		if (letters[i].specifier[0] == st[t])
+		{
+			return (letters[i].f);
+		}
+	}
+
+	return (0);
 }
