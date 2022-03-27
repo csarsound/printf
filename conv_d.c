0df0c00c6f62bb4efbeb1bd2cc45652d23219feb
csarsound <68489879+csarsound@users.noreply.github.com>
2022-03-27 08:27:48 -0500

    printf

--- conv_d.c
+++ conv_d.c
@@ -1,15 +1,20 @@
-/**/
+/**
+* conv_d - functio d
+* @n: enter
+* @letters: Struct
+* Return: char d
+*/
 int conv_d(int n, va_list letters)
 {
-        int nroDig = 0;
-        int auxi = n;
-        while (auxi>0)
-        {
-                auxi = auxi/10;
-                        nroDig++;
-        }
-        goWrite(nroDig);
-        return 0;
+int nroDig = 0;
+int auxi = n;
+
+while (auxi > 0)
+{
+auxi = (auxi / 10;
+nroDig++;
+}
+goWrite(nroDig);
+return (0);
 
 }
-
